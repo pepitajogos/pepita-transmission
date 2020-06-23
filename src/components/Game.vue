4c435457d42a68cf5a08179323b2e1ae3165ccbb
bedulatto <brunobedulatto@hotmail.com>
2020-06-23 16:03:06 -0300

    Update Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,48 +1,27 @@
 <template>
   <article class="game">
-    <Tutorial v-show="!tutorialFinished" ref="Tutorial" @finished="finishedTutorial" />
-    <SlidePlayer v-show="tutorialFinished && !gameFinished" @finished="finishedGame" />
+    <SlidePlayer @finished="finishedGame" />
     <RotationLock />
   </article>
 </template>
 
 <script>
-import Tutorial from "./Tutorial";
 import SlidePlayer from "./SlidePlayer";
 import RotationLock from "./RotationLock";
-const audio = require("@/audio.js");
 
 export default {
   name: "Game",
   data: () => ({
-    tutorialFinished: false,
     gameFinished: false
   }),
   components: {
-    Tutorial,
     SlidePlayer,
     RotationLock
   },
   methods: {
-    show() {
-      // console.log('Show Game');
-      this.$nextTick(() => {
-        this.$refs.Tutorial.show();
-      });
-    },
-    hide() {
-      // console.log('Hide Game');
-    },
-    finishedTutorial() {
-      this.tutorialFinished = !this.tutorialFinished;
-      this.musicPlay();
-    },
     finishedGame() {
       this.gameFinished = !this.gameFinished;
       this.$emit("ended"); // reset the home page
-    },
-    musicPlay() {
-      audio.playMusic("trilha1");
     }
   }
 };
